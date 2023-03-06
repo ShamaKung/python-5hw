c5be955b21203e9c32d91603da310458c74b6218
ShamaKung <shamaturin@gmail.com>
2023-03-06 11:25:42 +0500

    add task

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import re
+
 '''
 #Строка представляет собой арифметическое выражение из однозначных чисел и знаков + и -. Вычислите результат.
 
@@ -6,9 +7,34 @@ expression = input('Что вычислить?')
 print(eval(expression))
 '''
 
-
-
+'''
 s = 'My heart in the Highland'
 
 s2 = re.sub(r'\s+', '\n', s)
 print(s2)
+
+'''
+
+'''
+def power(base, exp):
+    if (exp == 1):
+        return (base)
+    if (exp != 1):
+        return (base * power(base, exp - 1))
+base = int(input("Введите число: "))
+exp = int(input("Введите его степень: "))
+print("Результат возведения в степень равен:", power(base, exp))
+'''
+
+
+def summa(a, b):
+    a += 1
+    b -= 1
+    if b > 0:
+        return summa(a, b)
+    else:
+        return a
+
+
+
+print(summa(2, 2))
